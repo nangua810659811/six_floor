74d894fe5f1beba081848a2bec1ee9483fdb3cac
renjunqu <renjunqu@126.com>
2015-11-04 20:41:57 +0800

    [rest testcase over]

--- unifyum-util/src/test/java/com/bupt/qrj/unifyum/util/test/http/HttpClientUtilTestCase.java
+++ unifyum-util/src/test/java/com/bupt/qrj/unifyum/util/test/http/HttpClientUtilTestCase.java
@@ -18,15 +18,23 @@ import com.bupt.qrj.unifyum.util.http.HttpClientUtil;
  */
 public class HttpClientUtilTestCase {
 
-	private final String RS_URL = "http://localhost:8080/unifyum/usermanagement.req?action=unregister";
+	private final String REGISTER_URL = "http://localhost:8080/unifyum/usermanagement.req?action=register";
+
+	private final String UNREGISTER_URL = "http://localhost:8080/unifyum/usermanagement.req?action=unregister";
 
 	@Test
 	public void testPost() {
-		assertTrue(true);
+
 		JSONObject rs_params = new JSONObject();
-		rs_params.put("userName", "qurenjun");
-		String httpRet = HttpClientUtil.doPostRequest(RS_URL, rs_params);
+		rs_params.put("userName", "qurenjun-test");
+		rs_params.put("password", "123456");
+		// 先做注册
+		String httpRet = HttpClientUtil.doPostRequest(REGISTER_URL, rs_params);
 		JSONObject rs_ret = JSONObject.parseObject(httpRet);
+		assertTrue(rs_ret.getBoolean("success"));
+		// 再做注销
+		httpRet = HttpClientUtil.doPostRequest(UNREGISTER_URL, rs_params);
+		rs_ret = JSONObject.parseObject(httpRet);
 		assertTrue(rs_ret.getBoolean("success"));
 	}
 }
