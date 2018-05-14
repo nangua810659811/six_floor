12fc4c0958218f6076b64dbd89a1d6d68243e4d2
nangua810659811 <song_shengnan@outlook.com>
2018-05-14 17:14:30 +0800

    original

--- water-dal/src/main/java/com/bupt/qrj/unifyum/dal/dao/workerloginDAO.java
+++ water-dal/src/main/java/com/bupt/qrj/unifyum/dal/dao/workerloginDAO.java
@@ -4,8 +4,6 @@
 package com.bupt.qrj.unifyum.dal.dao;
 
 
-import com.bupt.qrj.unifyum.dal.dataobject.workerLoginDO;
-
 /**
  * @author renjun.qrj 2015年10月31日:下午6:00:31 com.bupt.qrj.dao.impl.UserMetaDAO
  *         unifyum-dal 用途:
